4c6d83933f097b0df031d317144039596871704a
clairecontos1998 <43145713+clairecontos1998@users.noreply.github.com>
2019-08-01 14:28:33 -0400

    EmailjavaAPP

---
+++ EmailApp.java
@@ -0,0 +1,13 @@
+package emailApp;
+
+public class EmailApp {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		Email em1 = new Email("John", "Smith");
+		
+		em1.showInfo();
+		System.out.println(em1.showInfo());
+	}
+
+}
